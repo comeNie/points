c34b66fd11c329194210663c0049a7cc1ce479b1
lonnyxue <xueyunlong@guohuaigroup.com>
2017-03-16 18:40:02 +0800

    增加监听

--- src/main/java/com/guohuai/points/listener/ExampleListener.java
+++ src/main/java/com/guohuai/points/listener/ExampleListener.java
@@ -20,30 +20,30 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 public class ExampleListener {
 
-//	@Autowired
-//	private  鉴权接口;
-	
+	// @Autowired
+	// private 鉴权接口;
+
 	@Value("${integral_environment}")
 	String environment;
-	
+
 	@EventListener(condition = "#event.tradeType == '01'")
 	public void exampleEvent(ExampleEvent event) {
 		log.info("An event occured: {}", event);
 		String example = event.getExample();
-		if("test".equals(environment)){
+		if ("test".equals(environment)) {
 			example = "测试数据";
 		}
-		
+
 		/**
 		 * 接口功能实现
 		 */
-		
+
 		event.setReturnCode(Constant.FAIL);
 		// 判断是否鉴权成功
 		if (null != "") {
 			if ("0000".equals("")) {
 				event.setReturnCode(Constant.SUCCESS);
-			}else{
+			} else {
 				event.setErrorDesc("");
 				event.setReturnCode(event.getReturnCode());
 			}
