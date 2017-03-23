889b1782bdeea5b822aa3c3feed4fdf7aa2f19ef
glenny <huguolin@guohuaitech.com>
2017-03-23 09:45:27 +0800

    设置公共js：pointConfig.js

--- src/main/resources/static/assets/js/pointConfig.js
+++ src/main/resources/static/assets/js/pointConfig.js
@@ -1,20 +1,20 @@
-define(function(){
-	this.host = '';
-	return {
-		host: this.host,
-		api: {
-			pointGoods: {
-				save: this.host + '/points/goods/save',
-				update: this.host + '/points/goods/update',
-				edit: this.host + '/points/goods/edit',
-				list: this.host + '/points/goods/page'
-			},
-			pointSetting: {
-				save: this.host + '/points/setting/save',
-				update: this.host + '/points/setting/update',
-				edit: this.host + '/points/setting/edit',
-				list: this.host + '/points/setting/page'
-			}
-		}
-	}
+define(function () {
+    this.host = '';
+    return {
+        host: this.host,
+        api: {
+            pointGoods: {
+                save: this.host + '/points/goods/save',
+                update: this.host + '/points/goods/update',
+                edit: this.host + '/points/goods/edit',
+                list: this.host + '/points/goods/page'
+            },
+            purchaseBill: {
+                list: this.host + '/points/purchaseBill/page'
+            },
+            exchangedBill: {
+                list: this.host + '/points/exchangedBill/page'
+            }
+        }
+    }
 })
