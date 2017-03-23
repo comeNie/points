2c4a91aaf33012331421b5b617d2475678e8dc9f
glenny <huguolin@guohuaitech.com>
2017-03-23 15:31:28 +0800

    优化整理积分商品、购买积分设置模块代码

--- src/main/resources/static/pages/pointSetting/pointSetting.js
+++ src/main/resources/static/pages/pointSetting/pointSetting.js
@@ -103,90 +103,9 @@ define([
 						return res;
 					},
 					events: {
-						'click .style-delete': function(e, val, row){
-							//进入修改页面
-							console.log('oid===>'+row.oid);
-							var goodsOid = row.oid;
-							var confirm = $('#confirmModal');
-							confirm.find('.popover-title').html('提示');
-							confirm.find('p').html('确定要删除该积分产品吗？');
-							$('#tip_cancel').show();
-							$$.confirm({
-								container: confirm,
-								trigger: this,
-								accept: function(){
-									http.get(pointConfig.api.pointSetting.edit, {
-										data: {
-											oid: goodsOid,
-											state: -1
-										}, 
-									}, function(res){
-											if(res.errorCode == 0){
-												confirm.modal('hide');
-												$('#setting_Table').bootstrapTable('refresh', pageOptions);
-											}else{
-												errorHandle(res);//??
-											}
-									})
-								}
-							});
-						},
-						'click .style-putOn': function (e, val, row) {
-							//上架
-							var goodsOid = row.oid;
-							console.log('oid===>'+row.oid);
-							var confirm = $('#confirmModal');
-							confirm.find('.popover-title').html('提示');
-							confirm.find('p').html('确定要上架吗？');
-							$('#tip_cancel').show();
-							$$.confirm({
-								container: confirm,
-								trigger: this,
-								accept: function(){
-									http.get(pointConfig.api.pointSetting.edit, {
-										data: {
-											oid: goodsOid,
-											state: 1
-										},	
-									}, function(res){
-											console.log('errorCode=== '+ res.errorCode);
-											if(res.errorCode == 0){
-												confirm.modal('hide');
-												$('#setting_Table').bootstrapTable('refresh', pageOptions);
-											}else{
-												errorHandle(res);//??
-											}
-									})
-								}
-							});
-						},
-						'click .style-pullOff': function (e, val, row) {
-							//下架
-							var goodsOid = row.oid;
-							var confirm = $('#confirmModal');
-							confirm.find('.popover-title').html('提示');
-							confirm.find('p').html('确定要下架吗？');
-							$('#tip_cancel').show();
-							$$.confirm({
-								container: confirm,
-								trigger: this,
-								accept: function(){
-									http.get(pointConfig.api.pointSetting.edit, {
-										data: {
-											oid: goodsOid,
-											state: 2
-										},	
-									}, function(res){
-											if(res.errorCode == 0){
-												confirm.modal('hide');
-												$('#setting_Table').bootstrapTable('refresh', pageOptions);
-											}else{
-												errorHandle(res);//??
-											}
-									})
-								}
-							});
-						}
+						'click .style-delete': deleteSettingClick,
+						'click .style-putOn': putOnSettingClick,
+						'click .style-pullOff': pullOffSettingClick
 					}
 				}
 				]
@@ -196,6 +115,92 @@ define([
             $('#setting_Table').bootstrapTable(tableConfig);
             // 搜索表单初始化
             $$.searchInit($('#searchForm'), $('#setting_Table'));
+            
+            //按钮事件
+            function deleteSettingClick(e, val, row){
+				//进入修改页面
+				console.log('oid===>'+row.oid);
+				var goodsOid = row.oid;
+				var confirm = $('#confirmModal');
+				confirm.find('.popover-title').html('提示');
+				confirm.find('p').html('确定要删除该积分产品吗？');
+				$('#tip_cancel').show();
+				$$.confirm({
+					container: confirm,
+					trigger: this,
+					accept: function(){
+						http.get(pointConfig.api.pointSetting.edit, {
+							data: {
+								oid: goodsOid,
+								state: -1
+							}, 
+						}, function(res){
+								if(res.errorCode == 0){
+									confirm.modal('hide');
+									$('#setting_Table').bootstrapTable('refresh', pageOptions);
+								}else{
+									errorHandle(res);//??
+								}
+						})
+					}
+				});
+			}
+            function putOnSettingClick(e, val, row) {
+				//上架
+				var goodsOid = row.oid;
+				console.log('oid===>'+row.oid);
+				var confirm = $('#confirmModal');
+				confirm.find('.popover-title').html('提示');
+				confirm.find('p').html('确定要上架吗？');
+				$('#tip_cancel').show();
+				$$.confirm({
+					container: confirm,
+					trigger: this,
+					accept: function(){
+						http.get(pointConfig.api.pointSetting.edit, {
+							data: {
+								oid: goodsOid,
+								state: 1
+							},	
+						}, function(res){
+								console.log('errorCode=== '+ res.errorCode);
+								if(res.errorCode == 0){
+									confirm.modal('hide');
+									$('#setting_Table').bootstrapTable('refresh', pageOptions);
+								}else{
+									errorHandle(res);//??
+								}
+						})
+					}
+				});
+			}
+            function pullOffSettingClick(e, val, row) {
+				//下架
+				var goodsOid = row.oid;
+				var confirm = $('#confirmModal');
+				confirm.find('.popover-title').html('提示');
+				confirm.find('p').html('确定要下架吗？');
+				$('#tip_cancel').show();
+				$$.confirm({
+					container: confirm,
+					trigger: this,
+					accept: function(){
+						http.get(pointConfig.api.pointSetting.edit, {
+							data: {
+								oid: goodsOid,
+								state: 2
+							},	
+						}, function(res){
+								if(res.errorCode == 0){
+									confirm.modal('hide');
+									$('#setting_Table').bootstrapTable('refresh', pageOptions);
+								}else{
+									errorHandle(res);//??
+								}
+						})
+					}
+				});
+			}
             
             //列表自带搜索条件
             function getQueryParams(val) {
@@ -207,17 +212,6 @@ define([
 				return val
 			}
             
-            //搜索
-			$('#setting_search').on('click', function(e) {
-				e.preventDefault()
-				var sform = document.searchForm
-				var data = util.form.serializeJson(sform)
-				data.row = 10
-				data.page = 1
-				pageOptions = data
-				$('#setting_Table').bootstrapTable('refresh', pageOptions);
-			});
-			
 			// 点击新增事件
 			$('#setting_add').on('click', function() {
 				$('#addSettingModal').modal('show');
